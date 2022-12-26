56c486ba4d5c8df186e04d435e4ac15c8722746e
Ehsan Karimi <ehsankb91@gmail.com>
2022-12-26 10:53:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 from os import path
 
+VERSION = '0.1.17'
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -8,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='mimo_keras',
     packages=['mimo_keras'],
-    version='0.1.17',
+    version=VERSION,
     license='MIT',
     description='A DataGenerator for Keras multiple-input multiple-output models and massive datasets with any type of data',
     long_description=long_description,
@@ -16,7 +17,7 @@ setup(
     author='Simkarwin',
     author_email='simkarwin@gmail.com',
     url='https://github.com/simkarwin/mimo_keras',
-    download_url='https://github.com/simkarwin/mimo_keras/archive/V0.1.17.tar.gz',
+    download_url= f'https://github.com/simkarwin/mimo_keras/archive/refs/tags/v{VERSION}.tar.gz',
     keywords=['keras data generator', 'data generator', 'multi-input multi-output model', 'medical image processing'],
     install_requires=[
         'numpy>=1.0.0',
